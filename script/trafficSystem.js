768a1d80fe0fedaf37369ce3e23c2b527fff859f
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-01-11 11:08:47 +0530

    added feature to move car
    car class is created which is responsible to move car
    car is moving in one direction automatically

--- script/trafficSystem.js
+++ script/trafficSystem.js
@@ -24,15 +24,41 @@ const createGrid = function(){
   } 
 }
 
-const createCar = function(){
-  const location = [[57,45],[58,45],[59,45]];
-  location.forEach(([rowId, colId]) => {
-  const cell = document.getElementById(`${rowId}_${colId}`)
-  cell.classList.add('car');
-  });
+const drawCar = function(location){
+    location.forEach(([rowId, colId]) => {
+      const cell = getCell([rowId, colId])
+      cell.classList.add('car');
+    });
+  };
+
+const removeBack = function(location){
+  const [rowId, colId] = location[0];
+  const cell = getCell([rowId, colId])
+  cell.classList.remove('car');
+};  
+
+const getCell = ([rowId, colId]) => document.getElementById(`${rowId}_${colId}`);
+
+class Car{
+  constructor(location){
+    this.location = location;
+  }
+
+  moveCar(){
+    const [rowId, colId] = this.location[this.location.length-1];
+    this.location.shift(this.location[0])
+    this.location.push([rowId-1, colId])
+  }
 }
 
 const main = function(){
   createGrid();
-  createCar();
+  const location = [[59,45],[58,45],[57,45]];
+  drawCar(location);
+  const car = new Car(location);
+  setInterval(() => { 
+    removeBack(location)
+    car.moveCar();
+    drawCar(location);
+  }, 200);
 }
